bc3ee98133d2e49dc54656c3302f29411f470f02
johannajonsson <jojo.risin@outlook.com>
2026-04-30 13:58:49 +0200

    add dockerfile

--- src/main/java/se/jensen/johanna/fakestoregateway/config/SecurityConfig.java
+++ src/main/java/se/jensen/johanna/fakestoregateway/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
         .authorizeExchange(auth -> auth
             .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
             .pathMatchers("/api/auth/**").permitAll()
+            .pathMatchers("/api/products/**").permitAll()
             .anyExchange().authenticated()
         )
         .oauth2ResourceServer(oauth2 -> oauth2
