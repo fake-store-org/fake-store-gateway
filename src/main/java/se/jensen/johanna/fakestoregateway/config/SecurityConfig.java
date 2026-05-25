39564ac821dff8ba62cd8b6126d5b80953c6224b
johannajonsson <jojo.risin@outlook.com>
2026-05-25 12:53:11 +0200

    update deploy.yml for prod

--- src/main/java/se/jensen/johanna/fakestoregateway/config/SecurityConfig.java
+++ src/main/java/se/jensen/johanna/fakestoregateway/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
@@ -25,7 +26,7 @@ public class SecurityConfig {
   @Bean
   public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
     return http
-        .csrf(csrf -> csrf.disable())
+        .csrf(CsrfSpec::disable)
         .cors(
             cors -> cors.configurationSource(corsConfigurationSource()))
         .authorizeExchange(auth -> auth
@@ -33,7 +34,6 @@ public class SecurityConfig {
             .pathMatchers("/api/auth/**").permitAll()
             .pathMatchers("/api/products/**").permitAll()
             .pathMatchers("/api/reservations/check-stock").permitAll()
-            .pathMatchers("/api/orders/webhook").permitAll()
             .anyExchange().authenticated()
         )
         .oauth2ResourceServer(oauth2 -> oauth2
